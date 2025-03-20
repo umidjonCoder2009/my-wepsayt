cd5cfd35f3ff3f771e8c18dba64fe6afc41c9bdd
umidjonCoder2009 <umidjonfarxodov140@gmail.com>
2025-03-20 12:18:30 +0500

    contact qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./companents/navbar";
 import Services from "./pages/Services";
 import Blog from "./pages/Blog";
 import SHop from"./pages/SHop"
+import Contact from"./pages/Contact"
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/services" element={<Services />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/shop" element={<SHop/>} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
     </>
   );
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,4 @@
+function Contact() {
+  return <h2>HALU BU TAYOR BOLMAGAN</h2>;
+}
+export default Contact;
